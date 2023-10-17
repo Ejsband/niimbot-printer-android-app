d0c0acf8a6c3d60540754d65ff344668e91a6405
ELI <ivb.partner@yandex.ru>
2023-10-17 23:30:19 +0700

    git init

---
+++ app/src/main/java/ru/project/niimbot/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.project.niimbot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
