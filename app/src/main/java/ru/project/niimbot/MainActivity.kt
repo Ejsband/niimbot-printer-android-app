7580dab2f0f2e3906fa128d1c80e12272a017a5c
ELI <ivb.partner@yandex.ru>
2023-10-18 02:25:06 +0700

    niimbot implementation

--- app/src/main/java/ru/project/niimbot/MainActivity.kt
+++ app/src/main/java/ru/project/niimbot/MainActivity.kt
@@ -5,6 +5,7 @@ import android.bluetooth.BluetoothAdapter
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -12,9 +13,68 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.gengcon.www.jcprintersdk.JCPrintApi
+import com.gengcon.www.jcprintersdk.callback.Callback
+import com.gengcon.www.jcprintersdk.callback.PrintCallback
+import java.util.HashMap
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val mCallback = object : Callback {
+
+        override fun onConnectSuccess(p0: String?) {
+            Log.d("XXX", "Connection success")
+        }
+
+        override fun onDisConnect() {
+            Log.d("XXX", "Disconnected from device")
+        }
+
+        override fun onElectricityChange(p0: Int) {
+        }
+
+        override fun onCoverStatus(p0: Int) {
+        }
+
+        override fun onPaperStatus(p0: Int) {
+        }
+
+        override fun onRfidReadStatus(p0: Int) {
+        }
+
+        override fun onPrinterIsFree(p0: Int) {
+        }
+
+        override fun onHeartDisConnect() {
+        }
+
+        override fun onFirmErrors() {
+        }
+    }
+
+    private val printCallback = object : PrintCallback {
+        override fun onProgress(p0: Int, p1: Int, p2: HashMap<String, Any>?) {
+
+        }
+
+        override fun onError(p0: Int) {
+
+        }
+
+        override fun onError(p0: Int, p1: Int) {
+
+        }
+
+        override fun onCancelJob(p0: Boolean) {
+
+        }
+
+        override fun onBufferFree(p0: Int, p1: Int) {
+
+        }
+
+    }
 
     private val launcher =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { map ->
@@ -27,11 +87,17 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    private var jcapi: JCPrintApi = JCPrintApi.getInstance(mCallback)
+
     val bAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        jcapi.init(getApplication());
+        jcapi.initImageProcessingDefault("", "")
+
 
         val tvName = findViewById<TextView>(R.id.nameTv)
 
@@ -103,4 +169,7 @@ class MainActivity : AppCompatActivity() {
             add(Manifest.permission.ACCESS_COARSE_LOCATION)
         }.toTypedArray()
     }
+
+
+
 }
