92bf7ddf54ba339a249d593d53789e50b1e69384
ELI <ivb.partner@yandex.ru>
2023-10-19 01:13:44 +0700

    seventh try

--- app/src/main/java/ru/project/niimbot/MainActivity.kt
+++ app/src/main/java/ru/project/niimbot/MainActivity.kt
@@ -153,7 +153,7 @@ class MainActivity : AppCompatActivity() {
             jcapi.setTotalQuantityOfPrints(1)
             jcapi.startPrintJob(2, 3, 1, printCallback)
 //            jcapi.drawLabelImage(image, 50.0F, 50.0F, 50.0F, 50.0F, 0, 2, 127F)
-            jcapi.drawLabelText(20f, 10f, 40f, 10f, "Здрасте, я тута", "Бабуленька", 12 * 1.5F, 0, 1, 1, 6,
+            jcapi.drawLabelText(0f, 0f, 40f, 10f, "电量发⽣变化", "电量发⽣变化", 12 * 1.5F, 0, 1, 1, 6,
                 0F,
                 1F, arrayOf(false, false, false, false).toBooleanArray())
 
