5063ff617b9b67127c3c9137e4fd2df37a0b7b07
ilyabogdanov <ivb.partner@yandex.ru>
2023-10-24 22:58:22 +0700

    new method

--- app/src/main/java/ru/project/niimbot/ui/MainActivity.kt
+++ app/src/main/java/ru/project/niimbot/ui/MainActivity.kt
@@ -169,7 +169,7 @@ class MainActivity : AppCompatActivity() {
         val jsonInfo = "{\"printerImageProcessingInfo\": {\"orientation\": $orientation, \"margin\": [0,0,0,0], \"printQuantity\": $quantity, \"horizontalOffset\": 0, \"width\": $width, \"height\": $height, \"printMultiple\": $printMultiple, \"epc\": \"\"}}"
 
         infoList.add(jsonInfo)
-//        printer.drawEmptyLabel(width, height, orientation, "")
+        printer.drawEmptyLabel(width, height, orientation, "")
         val imageData: String = getJson(this, "image.json").replace("\"", "")
         printer.drawLabelImage(imageData, 0F, 0F, width, height, 0, 1, 127F)
         val jsonByte: ByteArray = printer.generateLabelJson()
